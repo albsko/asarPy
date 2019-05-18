3b9c235f331fe4d95e493c1e460ea2f3ee05b401
Albert <50720306+AS1337@users.noreply.github.com>
2019-05-18 21:06:13 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyAsar',
+    packages=['pyAsar'],
+    version='1.0',
+    author='AS1337',
+    author_email='as1337@gmail.com',
+    description='Library for working with .asar files',
+    long_description=open('README.rst').read(),
+    url='https://github.com/AS1337/pyAsar.git',
+    keywords=['asar', 'electron', 'pyAsar'],
+)
