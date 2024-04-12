62955e1724b900183ff35d5e828aad53a6df80bf
Albert Skonieczny <albertskonieczny@gmail.com>
2024-04-12 18:59:18 +0200

    rename

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='asarPy',
-    packages=['asarPy'],
-    version='1.0',
-    author='altsko',
-    author_email='albertskonieczny@gmail.com',
-    description='Library for working with .asar files',
+    name="asarPy",
+    packages=["asarPy"],
+    version="1.0",
+    author="albski",
+    author_email="albertskonieczny@gmail.com",
+    description="Library for working with .asar files",
     long_description=long_description,
-    url='https://github.com/altsko/asarPy.git',
-    keywords=['asar', 'electron', 'asarPy', 'pyasar'],
+    url="https://github.com/albski/asarPy.git",
+    keywords=["asar", "electron", "asarPy", "pyasar"],
 )
